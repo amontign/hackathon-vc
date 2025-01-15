b9dc4bc84fd5a512cc42f59d7ca28e11cfa117c8
Alban Montigny <alban25.montigny@gmail.com>
2025-01-15 13:49:23 +0100

    front-end update with loader

--- webapp/src/pages/api/market.js
+++ webapp/src/pages/api/market.js
@@ -1,3 +1,5 @@
+
+
 export default async (req, res) => {
     if (req.method !== 'GET') {
         return res.status(405).json({ message: 'Method not allowed' });
--- webapp/src/pages/index.jsx
+++ webapp/src/pages/index.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export default function Home() {
     const [market, setMarket] = useState('');
+    const [marketReasearched, setMarketReasearched] = useState(null);
+    const [searchResults, setSearchResults] = useState(false);
 
     function handleReasearch(e) {
         e.preventDefault();
+        if (!market) {
+            toast.error('Please enter a market');
+            return;
+        }
         fetch(`/api/market?text=${market}`)
         .then(response => {
             if (!response.ok) {
@@ -13,7 +20,10 @@ export default function Home() {
             return response.json();
         })
         .then(data => {
-            console.log(data);
+            setSearchResults(true);
+            e.target.children[0].blur();
+            setMarketReasearched(market);
+            setMarket('');
         }).catch((error) => {
             console.error('Error:', error);
         });
@@ -21,12 +31,21 @@ export default function Home() {
 
     return (
         <div className="w-full h-full relative">
-            <div className="w-full h-screen flex items-center justify-center flex-col gap-10">
-                <h1 className="text-white text-6xl">Market researcher</h1>
-                <form className="py-2 px-4 bg-white rounded-lg" onSubmit={handleReasearch}>
-                    <input type="text" className="outline-none w-96 text-2xl" placeholder="Search for a market" onChange={(e) => {setMarket(e.target.value)}} />
+            <div className={`w-full transition-all duration-700 ${searchResults ? "h-20 gap-0" : "h-screen gap-10"} flex items-center justify-center flex-col`}>
+                <h1 className={`text-white text-6xl pointer-events-none duration-500 transition-all ${searchResults ? "h-0 opacity-0" : "h-16 opacity-100"}`}>Market researcher</h1>
+                <form className="py-1 pl-4 pr-1 bg-white rounded-lg flex gap-2" onSubmit={handleReasearch}>
+                    <input type="text" className="outline-none w-96 text-2xl my-1" placeholder="Search for a market" value={market} onChange={(e) => {setMarket(e.target.value)}} />
+                    <button type="submit" className="bg-[#7E99A3] text-black text-2xl rounded px-2 transition-colors hover:bg-black hover:text-[#7E99A3]">Search</button>
                 </form>
             </div>
+            {searchResults && (
+                <div className="w-full h-full px-10">
+                    <p className="text-white text-3xl">Search results on {marketReasearched}</p>
+                    <div className="w-full h-[calc(100vh-10rem)] flex items-center justify-center">
+                        <div className="loader"></div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
